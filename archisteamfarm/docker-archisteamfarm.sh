1db9732a234230d88960ef2114618e3638a0a9c3
wcen <cew1350@gmail.com>
2018-10-05 10:37:46 +0800

    Remove container after deteching container

--- archisteamfarm/docker-archisteamfarm.sh
+++ archisteamfarm/docker-archisteamfarm.sh
@@ -3,7 +3,7 @@ set -e
 
 docker stop archisteamfarm || true && docker rm -vf archisteamfarm || true
 
-docker run -itd --restart=always --name archisteamfarm \
+docker run -itd --rm --name archisteamfarm \
   --cpus=1 --memory=256m \
   -v $(pwd)/config/bot.json:/app/config/bot.json \
   justarchi/archisteamfarm:latest
