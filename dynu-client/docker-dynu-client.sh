6e0ab794ac6a6bcf2e8621571c50884ffe68c1a6
wcen <cew1350@gmail.com>
2018-11-02 09:53:36 +0800

    Container name to parameterize

--- dynu-client/docker-dynu-client.sh
+++ dynu-client/docker-dynu-client.sh
@@ -1,4 +1,8 @@
-docker stop dynu-client > /dev/null 2>&1 || true && docker rm -vf dynu-client > /dev/null 2>&1 || true
-docker run -d --restart=always --name dynu-client \
+#!/bin/bash
+set -e
+CONTAINER_NAME="dynu-client"
+docker stop ${CONTAINER_NAME} > /dev/null 2>&1 || true && docker rm -vf ${CONTAINER_NAME} > /dev/null 2>&1 || true
+
+docker run -d --restart=always --name ${CONTAINER_NAME} \
   --env-file ./environment \
   wcen/docker-dynu-client:latest
