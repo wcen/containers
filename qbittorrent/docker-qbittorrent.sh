1add7b7eb827e81a1536fafdf266cf35f0a63a3c
wcen <cew1350@gmail.com>
2019-02-03 23:21:51 +0800

    Update docker-qbittorrent.sh

--- qbittorrent/docker-qbittorrent.sh
+++ qbittorrent/docker-qbittorrent.sh
@@ -1,10 +1,10 @@
 docker run -d --restart=always --name qbittorrent \
   --net bridge --net internal \
   --cpus=2 --memory=2048m \
-  -v $(pwd)/config:/config \
-  -v $(pwd)/data:/data \
-  -v $(pwd)/downloads:/downloads \
-  -v $(pwd)/watch:/watch \
+  -v $(pwd)/qbittorrent/config:/config \
+  -v $(pwd)/qbittorrent/data:/data \
+  -v $(pwd)/qbittorrent/downloads:/downloads \
+  -v $(pwd)/qbittorrent/watch:/watch \
   -p 8080:8080 \
   -p 8999:8999 \
   -p 8999:8999/udp \
